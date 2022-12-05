e617595693d17b83137a40536f9125cebd559b9e
Anamaria Groza <anamariagroza81@gmail.com>
2022-12-05 16:05:38 +0200

    created user and userContainer entity and updated controller

--- src/main/java/entities/UserContainer.java
+++ src/main/java/entities/UserContainer.java
@@ -4,4 +4,35 @@ public class UserContainer {
     private User user;
     private int id;
     private int serialNumber;
+
+    public UserContainer(User user, int id, int serialNumber) {
+        this.user = user;
+        this.id = id;
+        this.serialNumber = serialNumber;
+    }
+
+    public UserContainer() {
+    }
+
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
 }
